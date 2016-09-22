5594182a42a0e7eef1c7b605490b848acbaba4f7
Douglas Stebila <douglas@stebila.ca>
2016-09-22 17:00:46 -0400

    Fix sampling for "classical" (D2) parameters.

--- lwe_noise.h
+++ lwe_noise.h
@@ -25,9 +25,6 @@
 
 #include <stdint.h>
 
-// #define LWE_SAMPLE_N lwe_sample_n_binomial24 // samples from the binomial
-// #define LWE_SAMPLE_N lwe_sample_n_binomial32 // samples from the binomial
-
 #ifdef LWE_NOISE_D1
 #define LWE_SAMPLE_N lwe_sample_n_inverse_8
 #define LWE_CDF_TABLE CDF_D1
@@ -37,11 +34,11 @@ extern const uint8_t LWE_CDF_TABLE[];
 #endif
 
 #ifdef LWE_NOISE_D2
-#define LWE_SAMPLE_N lwe_sample_n_inverse_8
+#define LWE_SAMPLE_N lwe_sample_n_inverse_12
 #define LWE_CDF_TABLE CDF_D2
 #define LWE_CDF_TABLE_LENGTH CDF_LENGTH_D2
 
-extern const uint8_t LWE_CDF_TABLE[];
+extern const uint16_t LWE_CDF_TABLE[];
 #endif
 
 #ifdef LWE_NOISE_D3
